aeb6be5c56b4d277fe7a04dbec78abffcc3eadb5
kleinwalti <kleinwalti.facebook@googlemail.com>
2020-05-29 14:06:24 +0200

    Completing Manual Tank Movement

--- 04_BattleTank/BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ 04_BattleTank/BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -5,16 +5,24 @@
 
 void UTankMovementComponent::InitializeTracks(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-    if ( !LeftTrackToSet || !RightTrackToSet) {UE_LOG(LogTemp, Error, TEXT("No Left or No Right Track to Set! nullptr prot.")); return; }
+    if ( !LeftTrackToSet || !RightTrackToSet) {UE_LOG(LogTemp, Error, TEXT("No Left or No Right Track to Set! nullptr prot.")); return; }   // actually not really needed, but hey, some info if something is not right
     LeftTrack = LeftTrackToSet;
     RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-    UE_LOG(LogTemp, Warning, TEXT("%s Throw is: %f"), *GetName(), Throw );
+    // UE_LOG(LogTemp, Warning, TEXT("%s Throw is: %f"), *GetName(), Throw );   // delete
     if ( !LeftTrack || !RightTrack) {UE_LOG(LogTemp, Error, TEXT("No Left or No Right Track! nullptr prot.")); return; }
 
     LeftTrack->SetThrottle(Throw);
     RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+    if ( !LeftTrack || !RightTrack) {UE_LOG(LogTemp, Error, TEXT("No Left or No Right Track! nullptr prot.")); return; }
+
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(-Throw);
 }
--- 04_BattleTank/BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ 04_BattleTank/BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -23,8 +23,10 @@ public:
 	UFUNCTION ( BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
 
+	UFUNCTION ( BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
+
 private:
 	UTankTrack* LeftTrack = nullptr;
 	UTankTrack* RightTrack = nullptr;
-
 };
